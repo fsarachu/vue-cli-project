ad0e4d7003bb06f2bc50417a8e5bfd22eb5911f2
fsarachu <fsarachu@gmail.com>
2017-04-02 23:32:40 -0300

    Add name data and reverseName method

--- src/components/User/User.vue
+++ src/components/User/User.vue
@@ -4,9 +4,12 @@
             <div class="container">
                 <div class="box">
                     <h1 class="title has-text-centered">User</h1>
+
+                    <button @click="reverseName" class="button is-primary">Reverse Name</button>
+
                     <div class="columns">
                         <div class="column">
-                            <app-user-details name="Jack"></app-user-details>
+                            <app-user-details :name="name"></app-user-details>
                         </div>
                         <div class="column">
                             <app-user-edit></app-user-edit>
@@ -23,6 +26,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data: function () {
+            return {
+                name: 'Franco'
+            };
+        },
+        methods: {
+            reverseName() {
+                this.name = this.name.split('').reverse().join('');
+            }
+        },
         components: {
             appUserDetails: UserDetails,
             appUserEdit: UserEdit
